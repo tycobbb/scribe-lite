081631432ebfc0ed27af0e2bc664b64bc876e62e
Ty Cobb <ty.cobb.m@gmail.com>
2018-12-11 21:03:03 -0600

    wip round trip to rust ws server

--- api/src/main.rs
+++ api/src/main.rs
@@ -5,6 +5,26 @@ extern crate dotenv;
 extern crate rocket;
 extern crate ws;
 
+#[macro_use]
+extern crate serde_derive;
+extern crate serde;
+extern crate serde_json;
+
+#[derive(Deserialize, Debug)]
+struct Message<'a> {
+    name: &'a str,
+    data: Option<&'a str>
+}
+
+enum Error {
+    SocketFailed(ws::Error),
+    DecodeFailed(serde_json::Error)
+}
+
+fn decode<'a, T>(text: &'a str) -> Result<T, Error> where T: serde::Deserialize<'a> {
+    serde_json::from_str(text).map_err(Error::DecodeFailed)
+}
+
 fn main() {
     dotenv::dotenv()
         .ok();
@@ -12,8 +32,20 @@ fn main() {
     // start websocket listener
     std::thread::spawn(|| {
         ws::listen("127.0.0.1:8080", |out| {
-            move |msg| {
-                out.send(msg)
+            move |msg: ws::Message| {
+                let result = msg
+                    .as_text()
+                    .map_err(Error::SocketFailed)
+                    .and_then(decode::<Message>);
+
+                if let Ok(message) = result {
+                    match message.name {
+                        "STORY.JOIN" => out.send(ws::Message::text("joined")),
+                        _ => Ok(())
+                    }
+                } else {
+                    Ok(())
+                }
             }
         })
     });
--- web/src/Client/Socket.ts
+++ web/src/Client/Socket.ts
@@ -1,41 +1,72 @@
 import { InPort, OutPort } from "../App/Main"
 
 export class Socket {
-  private send: OutPort
-  private recv: InPort
+  private send:     OutPort
+  private recv:     InPort
+  private messages: any[];
+  private socket:   WebSocket | null
 
   constructor(send: OutPort, recv: InPort) {
-    this.send = send
-    this.recv = recv
+    this.send     = send
+    this.recv     = recv
+    this.messages = []
+    this.socket   = null
   }
 
   start() {
-    this.send.subscribe((data) => {
-      console.log("socket", "received:", data)
+    this.socket           = new WebSocket("ws://localhost:8080")
+    this.socket.onopen    = this.flush.bind(this)
+    this.socket.onmessage = this.handle.bind(this)
 
-      setTimeout(() => {
-        switch (data.name) {
-          case "STORY.JOIN":
-            this.respond({
-              name: "STORY.JOIN.DONE",
-              data: {
-                text: "This is the first line.",
-                name: "Mr. Socket"
-              }
-            }); break;
-          case "STORY.ADD_LINE":
-            this.respond({
-              name: "STORY.ADD_LINE.DONE",
-              data: null
-            }); break;
-          default: break;
-        }
-      }, 100)
-    })
+    this.send.subscribe(this.enqueue.bind(this))
+
+      // setTimeout(() => {
+      //   switch (data.name) {
+      //     case "STORY.JOIN":
+      //       this.respond({
+      //         name: "STORY.JOIN.DONE",
+      //         data: {
+      //           text: "This is the first line.",
+      //           name: "Mr. Socket"
+      //         }
+      //       }); break;
+      //     case "STORY.ADD_LINE":
+      //       this.respond({
+      //         name: "STORY.ADD_LINE.DONE",
+      //         data: null
+      //       }); break;
+      //     default: break;
+      //   }
+      // }, 100)
+    // })
   }
 
-  respond(payload: any) {
-    console.log("socket", "responding:", payload)
-    this.recv.send(payload)
+  // push messages
+  private enqueue(message: any) {
+    const state = this.socket!.readyState
+    if (state == WebSocket.CLOSED || state == WebSocket.CLOSING) {
+      return
+    }
+
+    this.messages.push(message)
+    console.debug("socket", "enqueued:", message)
+
+    if (state == WebSocket.OPEN) {
+      this.flush()
+    }
+  }
+
+  private flush() {
+    for (const message of this.messages) {
+      const json = JSON.stringify(message)
+      this.socket!.send(json)
+    }
+
+    this.messages = []
+  }
+
+  // handle events
+  private handle(event: MessageEvent) {
+    console.debug("socket", "received:", event.data)
   }
 }
