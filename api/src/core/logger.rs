c3ec49d485a2d0495a2a290a04230d4ed193b8e7
Ty Cobb <ty.cobb.m@gmail.com>
2019-03-03 16:41:07 -0600

    move logger setup

---
+++ api/src/core/logger.rs
@@ -0,0 +1,30 @@
+use std::io::Write;
+
+pub fn setup() {
+    let mut logs = env_logger::Builder::from_default_env();
+    logs.format(|buf, record| {
+        writeln!(buf, "[{}] {:<6} - {}",
+            format_level(record.level()),
+            format_module(record.module_path()),
+            record.args()
+        )
+    });
+
+    logs.init();
+}
+
+fn format_level<'a>(level: log::Level) -> &'a str {
+    match level {
+        log::Level::Error => "E",
+        log::Level::Warn  => "W",
+        log::Level::Info  => "I",
+        log::Level::Debug => "D",
+        log::Level::Trace => "T",
+    }
+}
+
+fn format_module<'a>(module: Option<&'a str>) -> &'a str {
+    module
+        .and_then(|module| module.split("::").next())
+        .unwrap_or("<?>")
+}
--- api/src/core/mod.rs
+++ api/src/core/mod.rs
@@ -3,3 +3,4 @@ pub mod sink;
 pub mod empty;
 pub mod errors;
 pub mod socket;
+pub mod logger;
